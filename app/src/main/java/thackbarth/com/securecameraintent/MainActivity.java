5e41483653f472f2abadd5561de012c1568d3f38
tomhackbarth <tomhackbarth@gmail.com>
2016-06-25 23:18:14 -0400

    Code Cleanup...

--- app/src/main/java/thackbarth/com/securecameraintent/MainActivity.java
+++ app/src/main/java/thackbarth/com/securecameraintent/MainActivity.java
@@ -3,7 +3,6 @@ package thackbarth.com.securecameraintent;
 import android.Manifest;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -13,7 +12,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
@@ -32,36 +30,30 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
-// code from: https://developer.android.com/training/camera/photobasics.html
+// code from:
 
 
 public class MainActivity extends AppCompatActivity
 {
     private static final String TAG = "MainActivity";
     private static final int REQUEST_IMAGE = 439;
-    static final int REQUEST_TAKE_PHOTO = 1;
+    private static final int REQUEST_TAKE_PHOTO = 1;
 
-    ImageView mImageView;
-    File destination;
-    String mCurrentPhotoPath;
-    String mCurrentPhotoPath1;
+    private ImageView mImageView;
+    private File destination;
+    private String mCurrentPhotoPath;
+
 
     // Storage Permissions
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
+    private static final String[] PERMISSIONS_STORAGE = {
             Manifest.permission.CAMERA,
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
 
-    /**
-     * Checks if the app has permission to write to device storage
-     *
-     * If the app does not has permission then the user will be prompted to grant permissions
-     *
-     * @param activity
-     */
-    public static void verifyStoragePermissions(Activity activity) {
+
+    private static void verifyStoragePermissions(Activity activity) {
         // Check if we have write permission
         int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
@@ -75,22 +67,9 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    public File getAlbumStorageDir(Context context, String albumName) {
-        // Get the directory for the app's private pictures directory.
-        File file = new File(context.getExternalFilesDir(
-                Environment.DIRECTORY_DCIM), albumName);
-        if (!file.mkdirs()) {
-            Log.e(TAG, "Directory not created");
-        }
-        return file;
-    }
-
-    public boolean isExternalStorageWritable() {
+    private boolean isExternalStorageWritable() {
         String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
+        return Environment.MEDIA_MOUNTED.equals(state);
     }
 
     @Override
@@ -107,6 +86,7 @@ public class MainActivity extends AppCompatActivity
                 .getExternalStorageDirectory(), "image.jpg");
 
         final Button button = (Button) findViewById(R.id.button_non_secure);
+        assert button != null;
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 takeNonPrivateImage();
@@ -114,6 +94,7 @@ public class MainActivity extends AppCompatActivity
         });
 
         final Button button1 = (Button) findViewById(R.id.button_secure);
+        assert button1 != null;
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 dispatchTakePictureIntent();
@@ -121,6 +102,7 @@ public class MainActivity extends AppCompatActivity
         });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -175,6 +157,7 @@ public class MainActivity extends AppCompatActivity
         String imageFileName = "JPEG_" + timeStamp + "_";
         //  /storage/emulated/0/Android/data/thackbarth.com.securecameraintent/files/Pictures/
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        assert storageDir != null;
         if ( !storageDir.exists() )
         {
             if (!storageDir.mkdir())
@@ -190,8 +173,8 @@ public class MainActivity extends AppCompatActivity
                 storageDir      /* directory */
         );
         // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = "file:/" + image.getAbsolutePath();
-        mCurrentPhotoPath1 =  image.getAbsolutePath();
+//        mCurrentPhotoPath = "file:/" + image.getAbsolutePath();
+        mCurrentPhotoPath =  image.getAbsolutePath();
         return image;
     }
 
@@ -263,7 +246,7 @@ public class MainActivity extends AppCompatActivity
         // Get the dimensions of the bitmap
         BitmapFactory.Options bmOptions = new BitmapFactory.Options();
         bmOptions.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(mCurrentPhotoPath1, bmOptions);
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
         int photoW = bmOptions.outWidth;
         int photoH = bmOptions.outHeight;
 
@@ -273,13 +256,13 @@ public class MainActivity extends AppCompatActivity
         // Decode the image file into a Bitmap sized to fill the View
         bmOptions.inJustDecodeBounds = false;
         bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inPurgeable = true;
+//        bmOptions.inPurgeable = true;
 
-        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath1, bmOptions);
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
         mImageView.setImageBitmap(bitmap);
     }
 
-    public void takeNonPrivateImage()
+    private void takeNonPrivateImage()
     {
         try
         {
