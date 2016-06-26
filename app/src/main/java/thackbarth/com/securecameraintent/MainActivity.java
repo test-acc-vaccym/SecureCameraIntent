34677f801e34fdf3759f2b62060fd10e9b3821ef
tomhackbarth <tomhackbarth@gmail.com>
2016-06-25 22:10:29 -0400

    Added 2 buttons for taking secure/non-secure pictures....

--- app/src/main/java/thackbarth/com/securecameraintent/MainActivity.java
+++ app/src/main/java/thackbarth/com/securecameraintent/MainActivity.java
@@ -1,8 +1,10 @@
 package thackbarth.com.securecameraintent;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -11,6 +13,7 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -18,6 +21,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
@@ -36,6 +40,34 @@ public class MainActivity extends AppCompatActivity
     ImageView mImageView;
     File destination;
 
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -49,6 +81,19 @@ public class MainActivity extends AppCompatActivity
         destination = new File(Environment
                 .getExternalStorageDirectory(), "image.jpg");
 
+        final Button button = (Button) findViewById(R.id.button_non_secure);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                takeNonPrivateImage();
+            }
+        });
+
+        final Button button1 = (Button) findViewById(R.id.button_secure);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener()
@@ -58,10 +103,14 @@ public class MainActivity extends AppCompatActivity
             {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
-                dispatchTakePictureIntent();
-//                takeNonPrivateImage();
+//                dispatchTakePictureIntent();
+                takeNonPrivateImage();
             }
         });
+
+
+        verifyStoragePermissions(this);
+
     }
 
     @Override
